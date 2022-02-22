ed752276092329dffe00d32e66e295a815dc0d95
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-02-22 19:09:53 +1000

    cleaned up cars route

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
 
-const index = () => {
+export default function CarsList(props) {
   return (
-    <h1>Cars List</h1>
-  )
-}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Cars list
+        </h1>
 
-export default index
+        <ul>
+          <li>
+            <Link href="/cars/tesla"><a>Tesla</a></Link>
+          </li>
+          <li>
+            <Link href="/cars/ford"><a>Ford</a></Link>
+          </li>
+          <li>
+            <Link href="/cars/lambo"><a>Lambo</a></Link>
+          </li>
+        </ul>
+        </main>
+    </div>
+  )
+  }
