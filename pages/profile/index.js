f5b91d77364621295d7bf81c3a0ba2b8e47e2c58
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-02-23 22:00:16 +1000

    duplicate code

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,7 +1,23 @@
 export default function Profile() {
     return (
-        <>
-            <div>This is the profile section</div>
-        </>
-    )
-}
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <h1 className={styles.title}>
+              Profile list
+            </h1>
+    
+            <ul>
+              <li>
+                <Link href="/cars/tesla"><a>Tesla</a></Link>
+              </li>
+              <li>
+                <Link href="/cars/ford"><a>Ford</a></Link>
+              </li>
+              <li>
+                <Link href="/cars/lambo"><a>Lambo</a></Link>
+              </li>
+            </ul>
+            </main>
+        </div>
+      )
+      }
